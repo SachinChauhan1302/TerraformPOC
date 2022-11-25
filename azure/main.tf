89e5d15b581b16bc2805744e2e9c6f7203da660c
SachinChauhan1302 <88776911+SachinChauhan1302@users.noreply.github.com>
2022-11-25 13:42:20 +0530

    Update main.tf

--- azure/main.tf
+++ azure/main.tf
@@ -3,7 +3,7 @@ provider "aws"{
   region = "ca-central-1"
 }
 
-resource "null_resource" "Hello2" {
+resource "null_resource" "Hello3" {
   provisioner "local-exec" {
     command = "echo Hello World by Terraform"
     }
